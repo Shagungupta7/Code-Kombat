af7e59050ef6ee0cd878a9dba4b8436411db353f
Shagun Gupta <shagungupta0307@gmail.com>
2025-10-25 01:51:48 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKy4LxCyiSSCFHrwGkqenbewA3W3t4cvo",
-  authDomain: "code-kombat.firebaseapp.com",
-  projectId: "code-kombat",
-  storageBucket: "code-kombat.firebasestorage.app",
-  messagingSenderId: "767870271269",
-  appId: "1:767870271269:web:7b2179d9443089d383e4cc",
-  measurementId: "G-VHB55Y9369"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
